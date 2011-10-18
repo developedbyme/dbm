61bc9f5527d9defb82fe04eadaa063862aa95709
Mattias Ekendahl <mattias@developedbyme.com>
2011-10-18 01:18:19 +0100

    UPDATE: tested nested alpha in canvas.

--- javascripts/examples/canvasViewExample.js
+++ javascripts/examples/canvasViewExample.js
@@ -7,15 +7,16 @@ dbm.runTempFunction(function() {
 		
 		var canvasView = CanvasView.create(document, true, "2d");
 		
-		canvasView.getController()._numberOfLinksToResolve = 15;
+		canvasView.getController()._numberOfLinksToResolve = 400;
 		
 		var centerLayer = canvasView.getController().getLayer("/center");
 		centerLayer.getProperty("x").setValue(100);
-		centerLayer.getProperty("y").setValue(50);
+		centerLayer.getProperty("y").setValue(70);
 		
 		var moveLayer = canvasView.getController().getLayer("/center/move");
 		moveLayer.getProperty("rotate").setValue(2*Math.PI/(canvasView.getController()._numberOfLinksToResolve+1));
-		moveLayer.getProperty("rotate").animateValue(2*Math.PI, 100, "linear");
+		moveLayer.getProperty("rotate").animateValue(2*Math.PI-0.005, 30, "linear");
+		moveLayer.getProperty("alpha").setValue(0.98);
 		
 		var graphicsLayer = canvasView.getController().getLayer("/center/move/graphics");
 		graphicsLayer.setStrokeStyle(0, "#FF0000");
