fd0bfb9857f27aa7146d2215eda0046f0099afd1
Mattias Ekendahl <mattias.ekendahl@b-reel.com>
2012-11-21 18:28:18 +0000

    ADDED: Added media query types.

---
+++ javascripts/dbm/classes/com/developedbyme/constants/css/MediaFeatureTypes.js
@@ -0,0 +1,20 @@
+dbm.registerClass("com.developedbyme.constants.css.MediaFeatureTypes", null, function(objectFunctions, staticFunctions, ClassReference) {
+	//console.log("com.developedbyme.constants.css.MediaFeatureTypes");
+	
+	var MediaFeatureTypes = dbm.importClass("com.developedbyme.constants.css.MediaFeatureTypes");
+	
+	MediaFeatureTypes.WIDTH = "width";
+	MediaFeatureTypes.HEIGHT = "height";
+	MediaFeatureTypes.DEVICE_WIDTH = "device-width";
+	MediaFeatureTypes.DEVICE_HEIGHT = "device-height";
+	MediaFeatureTypes.ORIENTATION = "orientation";
+	MediaFeatureTypes.ASPECT_RATIO = "aspect-ratio";
+	MediaFeatureTypes.DEVICE_ASPECT_RATIO = "device-aspect-ratio";
+	MediaFeatureTypes.COLOR = "color";
+	MediaFeatureTypes.COLOR_INDEX = "color-index";
+	MediaFeatureTypes.MONOCHROME = "monochrome";
+	MediaFeatureTypes.RESOLUTION = "resolution";
+	MediaFeatureTypes.SCAN = "scan";
+	MediaFeatureTypes.GRID = "grid";
+	
+});
---
+++ javascripts/dbm/classes/com/developedbyme/constants/css/MediaTypes.js
@@ -0,0 +1,17 @@
+dbm.registerClass("com.developedbyme.constants.css.MediaTypes", null, function(objectFunctions, staticFunctions, ClassReference) {
+	//console.log("com.developedbyme.constants.css.MediaTypes");
+	
+	var MediaTypes = dbm.importClass("com.developedbyme.constants.css.MediaTypes");
+	
+	staticFunctions.ALL = "all";
+	staticFunctions.BRAILLE = "braille";
+	staticFunctions.EMBOSSED = "embossed";
+	staticFunctions.HANDHELD = "handheld";
+	staticFunctions.PRINT = "print";
+	staticFunctions.PROJECTION = "projection";
+	staticFunctions.SCREEN = "screen";
+	staticFunctions.SPEECH = "speech";
+	staticFunctions.TTY = "tty";
+	staticFunctions.TV = "tv";
+	
+});
