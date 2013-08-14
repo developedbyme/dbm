40d8265fb411b8c77c57ce468937d7db90915e2e
Mattias Ekendahl <mattias.ekendahl@b-reel.com>
2013-08-14 20:20:35 +0100

    ADDED: Spline selection experiment created

---
+++ javascripts/experiments/classes/com/developedbyme/projects/experiments/splineselection/gui/Card.js
@@ -0,0 +1,51 @@
+dbm.registerClass("com.developedbyme.projects.experiments.splineselection.gui.Card", "com.developedbyme.gui.DisplayBaseObject", function(objectFunctions, staticFunctions, ClassReference) {
+	//console.log("com.developedbyme.projects.experiments.splineselection.gui.Card");
+	//"use strict";
+	
+	var Card = dbm.importClass("com.developedbyme.projects.experiments.splineselection.gui.Card");
+	
+	var ErrorManager = dbm.importClass("com.developedbyme.core.globalobjects.errormanager.ErrorManager");
+	var ReportTypes = dbm.importClass("com.developedbyme.constants.ReportTypes");
+	var ReportLevelTypes = dbm.importClass("com.developedbyme.constants.ReportLevelTypes");
+	
+	var ExternalCssVariableProperty = dbm.importClass("com.developedbyme.core.objectparts.ExternalCssVariableProperty");
+	
+	objectFunctions._init = function() {
+		console.log("com.developedbyme.projects.experiments.splineselection.gui.Card::_init");
+		
+		this.superCall();
+		
+		this._zIndex = this.addProperty("zIndex", ExternalCssVariableProperty.createWithoutExternalObject(this._objectProperty));
+		this._updateFunctions.getObject("display").addInputConnection(this._zIndex);
+		
+		return this;
+	};
+	
+	objectFunctions._connectObjectToZIndex = function() {
+		this._zIndex.setupExternalObject(this._element.getValue(), "z-index", null, 1);
+	};
+	
+	objectFunctions.setElement = function(aElement) {
+		
+		this.superCall(aElement);
+		
+		this._connectObjectToZIndex();
+		
+		return this;
+	};
+	
+	objectFunctions.setAllReferencesToNull = function() {
+		
+		
+		
+		this.superCall();
+	};
+	
+	staticFunctions.create = function() {
+		//console.log("com.developedbyme.projects.experiments.splineselection.gui.Card::create");
+		//console.log(aElement);
+		
+		var newCard = (new ClassReference()).init();
+		return newCard;
+	};
+});
