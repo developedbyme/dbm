6c3a31c4e422aeca2dffbbf6360c891ce483d431
Mattias Ekendahl <mattias.ekendahl@b-reel.com>
2015-06-02 17:18:01 +0200

    REFACTOR: Movied files into new file structure

---
+++ projects/experiments/happy-new-year/javascripts/configuration.js
@@ -0,0 +1,7 @@
+dbm.runTempFunction(function() {
+	
+	dbm.setup(window, document, "javascripts", "classes");
+	dbm.addBaseFolder("dbm", "../../../javascripts");
+	
+	dbm.addLibrary("CodeMirror", ["../../../../javascripts/libraries/codemirror/lib/codemirror.js", "../../../../javascripts/libraries/codemirror/mode/javascript/javascript.js"], "CodeMirror", ["../../../javascripts/libraries/codemirror/lib/codemirror.css"]);
+});
