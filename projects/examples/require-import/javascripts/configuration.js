43923791be31549d4c0d3f578130a832290fcd58
Mattias Ekendahl <mattias.ekendahl@b-reel.com>
2015-06-09 11:01:39 +0200

    REFACTOR: Moved files into new file structure

--- projects/examples/require-import/javascripts/configuration.js
+++ projects/examples/require-import/javascripts/configuration.js
@@ -2,9 +2,9 @@
 	requirejs.config(
 		{
 			"paths": {
-				"dbm/dbm": "../../../javascripts/dbm/dbmForRequireJs",
-				"dbm/setup": "../../../javascripts/dbm/setup",
-				"dbm": "../../../javascripts/dbm/classes/dbm",
+				"dbm/dbm": "../../../../javascripts/dbm/dbmForRequireJs",
+				"dbm/setup": "../../../../javascripts/dbm/setup",
+				"dbm": "../../../../javascripts/dbm/classes/dbm",
 				"Application": "classes/Application"
 			},
 			"map": {
