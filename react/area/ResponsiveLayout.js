1837aa8f95be55f0c71c967ef538c45120c62890
Mattias Ekendahl <mattias@developedbyme.com>
2025-01-10 16:24:26 +0100

    Update: added responsive layout

---
+++ react/area/ResponsiveLayout.js
@@ -0,0 +1,50 @@
+import Dbm from "../../index.js";
+import React from "react";
+
+export default class ResponsiveLayout extends Dbm.core.BaseObject {
+    _construct() {
+        super._construct();
+
+        this.item.setValue("widthElement", null);
+        
+        let elementSize = new Dbm.flow.updatefunctions.dom.ElementSize();
+        this.item.setValue("elementSize", elementSize);
+
+        elementSize.input.properties.element.connectInput(this.item.properties.widthElement);
+        elementSize.start();
+
+        let layoutSwitch = new Dbm.flow.updatefunctions.logic.RangeSwitch();
+        layoutSwitch.input.properties.value.connectInput(elementSize.output.properties.width);
+
+        this.item.setValue("layoutSwitch", layoutSwitch)
+
+        this.item.setValue("element", null);
+        this.item.properties.element.connectInput(layoutSwitch.output.properties.value);
+
+        let refToProperty = new Dbm.react.RefToProperty();
+        refToProperty.property = this.item.properties.widthElement;
+        this.item.setValue("ref/widthElement", refToProperty);
+
+        this.item.setValue("mainElement", <div ref={refToProperty}>
+            <Dbm.react.area.InsertElement element={this.item.properties.element} />
+        </div>);
+
+        console.log(">>>>>>>>>", this);
+    }
+
+    setDefaultLayout(aElement) {
+        this.item.layoutSwitch.input.defaultValue = React.createElement(React.Fragment, {"key": "default"}, aElement);
+
+        return this;
+    }
+
+    addLayout(aElement, aMinSize = null, aMaxSize = null) {
+        this.item.layoutSwitch.addValueForRange(React.createElement(React.Fragment, {"key": "layout_" + this.item.layoutSwitch.input.ranges.length}, aElement), aMinSize, aMaxSize);
+
+        return this;
+    }
+
+    get mainElement() {
+        return this.item.mainElement;
+    }
+}
--- react/area/index.js
+++ react/area/index.js
@@ -1,4 +1,14 @@
+import Dbm from "../../index.js";
+
 export {default as InsertElement} from "./InsertElement.js";
 export {default as HasData} from "./HasData.js";
 export {default as ScrollActivatedArea} from "./ScrollActivatedArea.js";
 export {default as OpenCloseExpandableArea} from "./OpenCloseExpandableArea.js";
+export {default as ResponsiveLayout} from "./ResponsiveLayout.js";
+
+export let responsiveLayout = function(aDefaultLayout) {
+    let newResponsiveLayout = new Dbm.react.area.ResponsiveLayout();
+    newResponsiveLayout.setDefaultLayout(aDefaultLayout);
+
+    return newResponsiveLayout;
+}
