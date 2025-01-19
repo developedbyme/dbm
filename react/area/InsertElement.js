b3599285f3cd4a4cc0346a388c115461ca9d786b
Mattias Ekendahl <mattias@developedbyme.com>
2025-01-19 15:11:47 +0100

    Update: getting element better

--- react/area/InsertElement.js
+++ react/area/InsertElement.js
@@ -10,7 +10,7 @@ export default class InsertElement extends Dbm.react.BaseObject {
         //console.log("InsertElement::render");
         //console.log(this);
         
-        let element = this.getDynamicProp("element").value;
+        let element = this.getPropValue("element");
 
         if(!element) {
             return React.createElement("div", {}, "No element set");
