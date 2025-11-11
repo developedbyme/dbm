3a505794834e80fd0b54be597bac56d32080404c
Mattias Ekendahl <mattias@developedbyme.com>
2025-11-11 09:13:15 +0100

    Update: not showing null or undefined texts

--- react/text/Text.js
+++ react/text/Text.js
@@ -1,4 +1,5 @@
 import Dbm from "../../index.js";
+import React from "react";
 
 export default class Text extends Dbm.react.BaseObject {
     _construct() {
@@ -6,7 +7,12 @@ export default class Text extends Dbm.react.BaseObject {
     }
 
     render() {
+        let rawText = this.getPropValue("text");
         let text = "" + this.getPropValue("text");
+
+        if(rawText === null || rawText === undefined) {
+            return React.createElement("span", {"data-no-text-value": text});
+        }
 
         return text;
     }
