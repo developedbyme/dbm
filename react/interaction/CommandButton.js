6dc3113c0becca9381b3764d29cee20e07545022
Mattias Ekendahl <mattias@developedbyme.com>
2025-11-26 10:12:11 +0100

    Update: used shared clone

--- react/interaction/CommandButton.js
+++ react/interaction/CommandButton.js
@@ -25,55 +25,6 @@ export default class CommandButton extends Dbm.react.BaseObject {
         }
     }
 
-    _performClone(aChild, aProps) {
-		
-		if(aChild instanceof Array) {
-			let returnArray = [];
-			
-			let currentArray = aChild;
-			let currentArrayLength = currentArray.length;
-			for(let i = 0; i < currentArrayLength; i++) {
-				let currentChild = currentArray[i];
-				returnArray.push(this._performClone(currentChild, aProps));
-			}
-			
-			return returnArray;
-		}
-		
-		let newProps = aProps;
-        /*
-        //MENOTE: this is a more generalized version, but we never pass in className in this function
-        if(aChild && aChild.props && aChild.props.className) {
-			newProps = {...aProps};
-            
-			if(aProps.className) {
-				newProps.className = aProps.className + " " + aChild.props.className;
-			} 
-			else {
-				newProps.className = aChild.props.className;
-			}
-		}
-        */
-		
-		
-		let callArray = [aChild, newProps];
-		
-		if(aChild && aChild.props) {
-			let firstChildChildren = aChild.props.children;
-			if(!firstChildChildren) {
-				//MENOTE: do nothing
-			}
-			else if(firstChildChildren instanceof Array) {
-				callArray = callArray.concat(firstChildChildren);
-			}
-			else {
-				callArray.push(firstChildChildren);
-			}
-		}
-		
-		return React.cloneElement.apply(React, callArray);
-	}
-
     _renderMainElement() {
 
         let children = Dbm.utils.ArrayFunctions.singleOrArray(this.getPropValue("children"));
@@ -89,7 +40,7 @@ export default class CommandButton extends Dbm.react.BaseObject {
                     replacedChildren.push(React.createElement("span", {"onClick": this._callback_clickBound, onKeyDown: CommandButton.callback_keyDown}, currentChild));
                 }
                 else {
-                    replacedChildren.push(this._performClone(currentChild, {"onClick": this._callback_clickBound, onKeyDown: CommandButton.callback_keyDown}));
+                    replacedChildren.push(Dbm.react.ChildFunctions.clone(currentChild, {"onClick": this._callback_clickBound, onKeyDown: CommandButton.callback_keyDown}));
                 }
             }
             else {
