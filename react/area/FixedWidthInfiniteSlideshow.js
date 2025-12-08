977b769ba6741715f57a85f2e16110681723767c
Mattias Ekendahl <mattias@developedbyme.com>
2025-12-08 15:51:11 +0100

    Update: ability to offset items

--- react/area/FixedWidthInfiniteSlideshow.js
+++ react/area/FixedWidthInfiniteSlideshow.js
@@ -5,7 +5,7 @@ export default class FixedWidthInfiniteSlideshow extends Dbm.react.BaseObject {
     _construct() {
         super._construct();
 		
-		
+		this._sliderItems = {};
 	}
 
     _removedUsedProps(aProps) {
@@ -13,6 +13,22 @@ export default class FixedWidthInfiniteSlideshow extends Dbm.react.BaseObject {
         delete aProps["prepareLength"];
         delete aProps["elementWidth"];
         delete aProps["spacing"];
+        delete aProps["index"];
+        delete aProps["startPositionOffset"];
+        delete aProps["itemClasses"];
+    }
+
+    _getItemForIndex(aIndex) {
+        let currentItem = this._sliderItems["i"+ aIndex];
+        if(!currentItem) {
+            currentItem = new Dbm.repository.Item();
+            currentItem.setValue("index", aIndex);
+            currentItem.setValue("envelope", 0);
+            currentItem.setValue("localPosition", 0);
+            this._sliderItems["i"+ aIndex] = currentItem;
+        }
+
+        return currentItem;
     }
 
     _renderMainElement() {
@@ -23,6 +39,8 @@ export default class FixedWidthInfiniteSlideshow extends Dbm.react.BaseObject {
         let prepLength = this.getPropValueWithDefault("prepareLength", 20);
         let elementWidth = this.getPropValueWithDefault("elementWidth", 200);
         let spacing = this.getPropValueWithDefault("spacing", 0);
+        let startPositionOffset = this.getPropValueWithDefault("startPositionOffset", 0);
+        let itemClasses = this.getPropValueWithDefault("itemClasses", "full-size");
 
         let index = this.getPropValue("index");
 
@@ -33,7 +51,7 @@ export default class FixedWidthInfiniteSlideshow extends Dbm.react.BaseObject {
         let stepLength = elementWidth+spacing;
         let movedLength = index*stepLength;
         
-        let startPosition = movedLength-prepLength;
+        let startPosition = movedLength-prepLength-startPositionOffset;
         let startIndex = Math.floor(startPosition/stepLength);
 
         let endPosition = (startPosition + viewWidth + 2*prepLength);
@@ -41,12 +59,21 @@ export default class FixedWidthInfiniteSlideshow extends Dbm.react.BaseObject {
 
         let children = [];
         for(let i = startIndex; i <= endIndex; i++) {
-            let currentPosition = i*stepLength-movedLength;
+            let currentPosition = i*stepLength-movedLength+startPositionOffset;
             let elementIndex = Dbm.utils.NumberFunctions.floatMod(i, numberOfElements);
 
-            //METODO: send in styles instead of forcing full size
+            let indexItem = this._getItemForIndex(i);
+            let localPosition = i-index;
+            indexItem.localPosition = localPosition;
+            let envelope = 1-Math.min(1, Math.abs(localPosition));
+            indexItem.envelope = envelope;
+
             let style = {"transform": "translateX(" + currentPosition + "px)", position: "absolute", left: 0, top: 0};
-            let child = React.createElement("div", {"key": i, "className": "full-size", "style": style}, elements[elementIndex])
+            let child = React.createElement("div", {"key": i, "className": itemClasses, "style": style},
+                React.createElement(Dbm.react.context.AddContextVariables, {"values": {"sliderItem": indexItem}},
+                    elements[elementIndex]
+                )
+            );
             children.push(child);
         }
         
